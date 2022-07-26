307d8e1debc263d44564156ca887b883f035c6e9
carfiguru@hotmail.com <carfiguru@hotmail.com>
2022-07-25 21:51:48 -0500

    some js build with some connector functions

---
+++ db/db.js
@@ -0,0 +1,28 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',   
+    database: 'company'
+});
+
+const db = {
+    viewDept: function () {
+        return connection.promise().query(
+            'SELECT * FROM `department`'
+        );
+    },
+    viewRole: function () {
+        return connection.promise().query(
+            'SELECT * FROM `role`'
+        );
+    },
+    viewEmployee: function () {
+        return connection.promise().query(
+            'SELECT * FROM `employee` join `role` on employee.role_id = `role`.id join department on role.department_id = department.id'
+        );
+    },
+}
+
+module.exports = db
--- index.js
+++ index.js
@@ -1,65 +1,163 @@
+const db = require('./db/db.js');
 
 const inquirer = require("inquirer");
 
-const mysql = require('mysql2');
-
 const cTable = require('console.table');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'eTracker'
-  });
 
-const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: "what is the title of your readme"
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: "leave a brief description"
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: "Add some installation instructions"
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: "What is the application used for?"
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: "Select License",
-        choices: [
-            'Apache',
-            'Boost',
-            'BSD'
-        ]
-    },
-    {
-        type: 'input',
-        name: 'contributing',
-        message: "Are there any contributors on this project?"
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: "Do you have any tests for your application"
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: "Do you have a Github you'd like to add"
-    }, 
-    {
-        type: 'input',
-        name: 'email',
-        message: "Please include your email address."
-    },
-];
+db.viewEmployee().then(data =>{
+    console.log(data[0]);
+});
+
+//// TODO: Create an array of questions for user input
+// const questions = [
+//     {
+//         type: 'input',
+//         name: 'employeeName',
+//         message: "What is the team employee's name?"
+//     },
+//     {
+//         type: 'list',
+//         name: 'employeeTitle',
+//         message: "What is the title of the employee?",
+//         choices: [
+//             'Manager',
+//             'Engineer',
+//             'Intern'
+//         ]
+//     },
+//     {
+//         type: 'input',
+//         name: 'employeeId',
+//         message: "What is the employee's id?"
+//     },
+//     {
+//         type: 'input',
+//         name: 'employeeEmail',
+//         message: "What is the employee's email address?"
+//     }];
+
+// const managerQuestion = {
+//     type: 'input',
+//     name: 'office',
+//     message: "What is the manager's office number?"
+// };
+
+// const exitQuestion = {
+//     type: 'list',
+//     name: 'chooseNext',
+//     message: "What would you like to do next?",
+//     choices: [
+//         'Add Engineer',
+//         'Add Intern',
+//         'Exit'
+//     ]
+// };
+
+// const internQuestion = {
+//     type: 'input',
+//     name: 'school',
+//     message: "What is the intern's school?"
+// };
+
+// const engineerQuestion = {
+//     type: 'input',
+//     name: 'gUser',
+//     message: "What is the engineer's github username?"
+// };
+
+// TODO: Create a function to initialize app
+// function init() {
+//     var managerQuestions = [...questions, managerQuestion, exitQuestion]
+//     inquirer.prompt(managerQuestions)
+//         .then((answers) => {
+//             var manager = new Manager(answers.employeeName, answers.employeeTitle, answers.employeeId, answers.employeeEmail, answers.office);
+//             employees.push(manager)
+//             if (
+//                 answers.chooseNext === 'Add Engineer'
+//             ) {
+//                 addEngineer();
+//             } else if (
+//                 answers.chooseNext === 'Add Intern'
+//             ) {
+//                 addIntern();
+//             } else if (
+//                 answers.chooseNext === 'Exit'
+//             ) {
+//                 var data = renderData(employees);
+//                 writeToFile("dist/index.html", data);
+//             }
+//         })
+//         .catch((error) => {
+//             if (error.isTtyError) {
+//                 // Prompt couldn't be rendered in the current environment
+//             } else {
+//                 // Something else went wrong
+//             }
+//         });
+// }
+
+//promt user for engineer specific question
+// function addEngineer() {
+//     var engineerQuestions = [...questions, engineerQuestion, exitQuestion]
+//     inquirer.prompt(engineerQuestions)
+//         .then((answers) => {
+//             var engineer = new Engineer(answers.employeeName, answers.employeeTitle, answers.employeeId, answers.employeeEmail, answers.gUser);
+//             employees.push(engineer);
+//             if (
+//                 answers.chooseNext === 'Add Engineer'
+//             ) {
+//                 addEngineer();
+//             } else if (
+//                 answers.chooseNext === 'Add Intern'
+//             ) {
+//                 addIntern();
+//             } else if (
+//                 answers.chooseNext === 'Exit'
+//             ) {
+//                 var data = renderData(employees);
+//                 writeToFile("dist/index.html", data);
+//             }
+//         })
+//         .catch((error) => {
+//             if (error.isTtyError) {
+//                 // Prompt couldn't be rendered in the current environment
+//             } else {
+//                 // Something else went wrong
+//             }
+//         });
+// };
+
+// //promt user for intern specific question
+// function addIntern() {
+//     var internQuestions = [...questions, internQuestion, exitQuestion]
+//     inquirer.prompt(internQuestions)
+//         .then((answers) => {
+//             var intern = new Intern(answers.employeeName, answers.employeeTitle, answers.employeeId, answers.employeeEmail, answers.school);
+//             employees.push(intern);
+//             if (
+//                 answers.chooseNext === 'Add Engineer'
+//             ) {
+//                 addEngineer();
+//             } else if (
+//                 answers.chooseNext === 'Add Intern'
+//             ) {
+//                 addIntern();
+//             } else if (
+//                 answers.chooseNext === 'Exit'
+//             ) {
+//                 var data2 = renderData(employees);
+//                 writeToFile("dist/index.html", data2);
+//             }
+//         })
+//         .catch((error) => {
+//             if (error.isTtyError) {
+//                 // Prompt couldn't be rendered in the current environment
+//             } else {
+//                 // Something else went wrong
+//             }
+//         });
+// };
+
+// // Function call to initialize app
+// init();
