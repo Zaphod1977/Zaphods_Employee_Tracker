da29f38653e79fbbcf3f9b76cb510cc0e3c82fae
carfiguru@hotmail.com <carfiguru@hotmail.com>
2022-07-26 14:41:34 -0500

    finish seed population

--- db/seed.sql
+++ db/seed.sql
@@ -2,18 +2,25 @@ INSERT INTO employee
   (first_name, last_name, role_id, manager_id)
 VALUES
   ('Zaphod', 'Bobbybrox', 1, 1);
--- insert more seed info x 5
+  ('Tricia', 'McMillian', 2, 0);
+  ('Ford', 'Prefect', 3, 2);
+  ('Arthur', 'Dent', 3, 0);
+  ('Marvin', 'Galacticbrain', 4, 0);
 
   INSERT INTO `role`
   (title, salary, department_id)
 VALUES
-  ('Owner', 200000.00, 1);
--- insert more seed info x 5
+  ('Ship Captain', 20000000.00, 1);
+  ('Earthling', 10000.00, 1);
+  ('Journalist', 1000.00, 2);
+  ('Earthling', 1000.00, 2);
+  ('Primary Robot', 100.00, 1);
 
   INSERT INTO department
   (`name`)
 VALUES
-  ('Client aquasition');
-  -- insert more seed info x 5
-
-  
+  ('Ship Crew');
+  ('Ship Crew');
+  ('Hitchhiker');
+  ('Hitchhiker');
+  ('Ship Crew')
